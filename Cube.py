450ee314c4837cf9093bd0b0193ac48d0cb644b3
PCFIXE <pilou.tondon@hotmail.fr>
2021-11-22 14:23:29 +0100

    Classe representant une case

--- Cube.py
+++ Cube.py
@@ -1,4 +1,5 @@
 import pygame
+pygame.font.init()
 
 class Cube:
 	rows=9
@@ -9,20 +10,27 @@ class Cube:
 		self.row=row
 		self.col=col
 		self.selected=False
+		self.temp=0
 
 	def draw(self, win):
-		fnt = pygame.font.SysFont("comicsans", 40)
+		fnt = pygame.font.SysFont("comicsans", 36)
 
-		gap = self.width / 9
+		gap = 540 / 9
 		x = self.col * gap
 		y = self.row * gap
 
-		if self.selected:
-			pygame.draw.rect(win, (255,0,0), (x,y, gap ,gap), 3)
-		
+		if self.value == 0:
+			text = fnt.render(str(self.temp), 1, (128,128,128))
+			win.blit(text, (x+5, y+5))
 		if not(self.value == 0):
 			text = fnt.render(str(self.value), 1, (0, 0, 0))
 			win.blit(text, (x + (gap/2 - text.get_width()/2), y + (gap/2 - text.get_height()/2)))
 
+		if self.selected:
+			pygame.draw.rect(win, (255,0,0), (x,y, gap ,gap), 3)
+
 	def set(self, val):
 		self.value = val
+
+	def set_temp(self, val):
+		self.temp = val
